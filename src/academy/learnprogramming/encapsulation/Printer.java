16f5b37cda8f8e2187a3c6b17d129f5358c98d8f
Chintan Patel <chintan737@gmail.com>
2020-04-13 13:02:00 +0530

    printer program of encapsulation concepts

---
+++ src/academy/learnprogramming/encapsulation/Printer.java
@@ -0,0 +1,48 @@
+package academy.learnprogramming.encapsulation;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100)
+        {
+            this.tonerLevel = tonerLevel;
+        } else
+        {
+            this.tonerLevel = -1;
+        }
+
+
+
+        this.pagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if (this.tonerLevel + tonerAmount > 100) {
+                return -1;
+            }
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
+        } else
+        {
+            return -1;
+
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesToPrint /= 2;
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+
+}
